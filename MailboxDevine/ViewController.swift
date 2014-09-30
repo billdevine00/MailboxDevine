b0c692881687817d495a01b862d9cec5d6d8c0a9
WilliamDevine <williamdevine@mac.com>
2014-09-29 18:17:28 -0700

    Before refactoring to include velocity

--- MailboxDevine/ViewController.swift
+++ MailboxDevine/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var archiveIcon: UIImageView!
     @IBOutlet weak var listIcon: UIImageView!
     @IBOutlet weak var feedImage: UIImageView!
+    @IBOutlet var edgeGestureRecognizer: UIScreenEdgePanGestureRecognizer!
 
     var imageCenter: CGPoint!
 
@@ -56,6 +57,12 @@ class ViewController: UIViewController {
             println(messageImageView.center.x)
 
             messageImageView.center.x = translation.x + imageCenter.x
+            deleteIcon.center.x = translation.x - 30
+            laterIcon.center.x = translation.x + 350
+            archiveIcon.center.x = translation.x - 30
+            listIcon.center.x = translation.x + 350
+
+            
             // SWIPE LEFT. Turning the background yellow and showing the later icon
             
             if (messageImageView.center.x <= 100) {
@@ -91,36 +98,60 @@ class ViewController: UIViewController {
                 
             }
             
+//            if (messageImageView.center.x <= 219) {
+//                println("omg4")
+//               
+//                msgView.backgroundColor = UIColor.grayColor()
+//                archiveIcon.alpha = 0
+//            }
+            
         } else if gestureRecognizer.state == UIGestureRecognizerState.Ended{
             println("pan ended")
             msgView.backgroundColor = UIColor.grayColor()
             
             UIView.animateWithDuration(0.2, animations: {() -> Void in
                 
+                
                 if self.messageImageView.center.x >= 220 && self.messageImageView.center.x <= 319 {
-                    println("center is between 220 and 319")
-                    self.performSegueWithIdentifier("rescheduleSegue", sender: self)
+                    println("center ended is between 220 and 319")
+                    self.messageImageView.center.x = 640
+                    self.msgView.backgroundColor = UIColor.greenColor()
+                    self.msgView.alpha = 0
+                    self.feedImage.frame.origin.y = 144
+                    self.scrollView.contentSize = CGSizeMake(320,1994)
                 }
                 
                 if self.messageImageView.center.x >= 320  {
-                    println("center is past 320")
+                    println("center ended is past 320")
                     self.messageImageView.center.x = 640
                     self.msgView.backgroundColor = UIColor.redColor()
                     self.msgView.alpha = 0
                     self.feedImage.frame.origin.y = 144
-                    
+                    self.scrollView.contentSize = CGSizeMake(320,1994)
+
                     
                 }
-//
-//                if velocity.x < 220  {
-//                    self.messageImageView.center.x = 160
-//                    
-//                }
-//                
-//                if velocity.x < 100  {
-//                    self.messageImageView.center.x = 160
-//                    
-//                }
+
+                if self.messageImageView.center.x <= 100 && self.messageImageView.center.x >= 1  {
+                    println("center ended is less than 101 greater than 1")
+
+                      self.performSegueWithIdentifier("rescheduleSegue", sender: self)
+                    self.msgView.backgroundColor = UIColor.yellowColor()
+                    self.messageImageView.center.x = 160
+
+                }
+                
+                if self.messageImageView.center.x <= 101   {
+                    println("center ended is less than 100")
+                    self.performSegueWithIdentifier("listSegue", sender: self)
+                    self.messageImageView.center.x = 160
+                    
+                }
+                    if self.messageImageView.center.x <= 219 {
+                    println("center ended is between 220 and 319")
+                    self.messageImageView.center.x = 160
+                        
+                }
                 
                 
                 })
